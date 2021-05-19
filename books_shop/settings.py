cbaf99aea18b237980edffa942c26986777d4f34
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-05-19 21:28:00 +0300

    init

--- books_shop/settings.py
+++ books_shop/settings.py
@@ -104,15 +104,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = 'Y-m-d'
 
 
 # Static files (CSS, JavaScript, Images)
--- sales_manager/urls.py
+++ sales_manager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from sales_manager import views
 
 urlpatterns = [
-    path('', views.main_page)
+    path('', views.main_page),
+    path('book_detail/<int:book_id>/', views.book_detail, name="book-detail")
 ]
